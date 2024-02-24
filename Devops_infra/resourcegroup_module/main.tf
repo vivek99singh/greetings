6c45fb0dcb588994096deae84673212bf07c450b
Vivek Singh <vivek.99.singh@gmail.com>
2024-02-25 01:09:10 +0200

    removed the extra code in respective modules

--- Devops_infra/resourcegroup_module/main.tf
+++ Devops_infra/resourcegroup_module/main.tf
@@ -2,7 +2,3 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
-
-output "resource_group_id" {
-  value = azurerm_resource_group.rg.id
-}
--- Devops_infra/storageaccount_module/main.tf
+++ Devops_infra/storageaccount_module/main.tf
@@ -7,7 +7,3 @@ resource "azurerm_storage_account" "storage" {
 
 
 }
-
-output "storage_account_primary_connection_string" {
-  value = azurerm_storage_account.storage.primary_connection_string
-}
